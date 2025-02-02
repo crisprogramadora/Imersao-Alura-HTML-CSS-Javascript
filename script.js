e2eeac30ce62c52411d661fa0d5512e9ddb267b6
Cristiane Sá <crisprogramadora24@outlook.com>
2025-02-02 14:02:45 -0300

    Aula 4

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//scripts
+const resultadoArtista = document.getElementById("resultado-artista");
+const resultadoPlaylists = document.getElementById('resultado-playlists');
+const inputPesquisa = document.getElementById('input-pesquisa');
+
+function requestApi(termoPesquisa) {
+    fetch(`http://localhost:3000/artistas?nome_like=${termoPesquisa}`)
+        .then((response) => response.json())
+        .then((result) => exibirResultados(result))
+}
+
+function exibirResultados(result) {
+    resultadoPlaylists.classList.add("hidden");
+    const nomeArtista = document.getElementById('nome-artista');
+    const imagemArtista = document.getElementById('img-artista');
+
+    result.forEach((element) => {
+        nomeArtista.innerText = element.nome;
+        imagemArtista.src = element.urlImg;
+    });
+
+    resultadoArtista.classList.remove("hidden");
+}
+
+inputPesquisa.addEventListener("input", function () {
+    const termoPesquisa = inputPesquisa.value.toLowerCase();
+    if (termoPesquisa === "") {
+        resultadoPlaylists.classList.add("hidden");
+        resultadoArtista.classList.remove("hidden");
+        return;
+    }
+    requestApi(termoPesquisa);
+});
